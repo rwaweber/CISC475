e9b58d3107cf2e0dc5542e9be8248f86577ca928
gmohler213 <gmohler@udel.edu>
2016-02-23 13:29:45 -0500

    added J Unit test case

--- src/Source/InputOutput.java
+++ src/Source/InputOutput.java
@@ -3,5 +3,13 @@ package Source;
 /* Class for taking in JSON data files and transforming
 into stream objects. */
 public class InputOutput {
+	
+	public void InputParser(){
+		
+	}
+	
+	public static void main(String[] args){
+		
+	}
 
 }
