c8481a477851867af37e43d265cde96152bc5dd9
Ben Rodd <brodd@udel.edu>
2016-02-23 13:48:13 -0500

    ...

--- src/Source/FileParser.java
+++ src/Source/FileParser.java
@@ -2,13 +2,15 @@ package Source;
 
 /* Class for taking in JSON data files and transforming
 into stream objects. */
-public class InputOutput {
+public class FileParser {
 	
 	public void InputParser(){
 		
 	}
-	
+		
 	public static void main(String[] args){
+		
+		System.out.println("test");
 		
 	}
 
--- src/Tests/FileParserTest.java
+++ src/Tests/FileParserTest.java
@@ -4,11 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class IOTest {
+public class FileParserTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testJSONToStream() {
+		
 	}
+	
+	
 
 }
