bdfe3cc6c685e58b036bc4bd5c8edd449b1e68ac
Ben Rodd <brodd@udel.edu>
2016-02-24 18:40:34 -0500

    added and tested a method in FileParser that converts a list of streams into an array of JSON objects

--- src/main/FileParser.java
+++ src/main/FileParser.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.util.List;
+import java.util.stream.Stream;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 /* Class for taking in JSON data files and transforming
 into stream objects. */
 public class FileParser {
@@ -13,5 +18,22 @@ public class FileParser {
 		System.out.println("test");
 		
 	}
+	
+	public static JSONObject[] streamToJSON(List<Stream> streams, List<String> colNames, int numRows){
+		JSONObject[] jsonObjects = new JSONObject[numRows];
+		for(int i = 0; i < jsonObjects.length; i++){
+			jsonObjects[i] = new JSONObject();
+		}
+		for(int j = 0; j < streams.size(); j++){
+			Object[] thisStream = streams.get(j).toArray();
+			for(int k = 0; k < numRows; k++){
+				jsonObjects[k].put(colNames.get(j), thisStream[k]);
+			}
+		}	
+		return jsonObjects;
+		
+	}
+	
+	
 
 }
--- src/tests/FileParserTest.java
+++ src/tests/FileParserTest.java
@@ -2,13 +2,51 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.json.simple.JSONObject;
 import org.junit.Test;
+
+import main.FileParser;
 
 public class FileParserTest {
 
 	@Test
 	public void testJSONToStream() {
 		
+	}
+	
+	@Test
+	public void testStreamToJSON(){
+				
+		List<String> colNames = Arrays.asList(new String[]{"Name", "Age", "ID"});
+		
+		List<String> names = Arrays.asList(new String[]{"Bob", "Mary", "Albert"});
+		List<Integer> ages = Arrays.asList(new Integer[]{32, 40, 27});
+		List<Integer> ids = Arrays.asList(new Integer[]{12345, 98765, 11223});
+		
+		Stream<String> namesStream = names.stream();
+		Stream<Integer> agesStream = ages.stream();
+		Stream<Integer> idsStream = ids.stream();
+		
+		List<Stream> streams = new ArrayList<Stream>();
+		streams.add(namesStream);
+		streams.add(agesStream);
+		streams.add(idsStream);
+		
+		int numRows = 3;
+		
+		JSONObject[] jsonObjects = FileParser.streamToJSON(streams, colNames, numRows);
+		
+		assertNotNull(jsonObjects);
+		assertEquals(jsonObjects.length, 3);
+		assertEquals(jsonObjects[0].get("Name"), "Bob");
+		assertEquals(jsonObjects[1].get("Age"), 40);
+		assertEquals(jsonObjects[2].get("ID"), 11223);
+
 	}
 	
 	
