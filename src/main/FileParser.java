43af7dd14413c7a6553b7b8f5bab3515918ab488
gmohler213 <gmohler@udel.edu>
2016-02-26 12:29:03 -0500

    Added JSON Object list to Stream method.

--- src/main/FileParser.java
+++ src/main/FileParser.java
@@ -14,14 +14,9 @@ import org.json.simple.JSONObject;
 into stream objects. */
 public class FileParser {
 	
-	public void InputParser(){
-		
-	}
-		
-	public static void main(String[] args){
-		
-		System.out.println("test");
-		
+	public Stream<JSONObject> jsonToStream(JSONObject[] list){
+		Stream<JSONObject> stream = Stream.of(list);
+		return stream;
 	}
 	
 	public static JSONObject[] streamToJSON(List<Stream> streams, List<String> colNames, int numRows){
@@ -59,6 +54,12 @@ public class FileParser {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public static void main(String[] args){
+
+		System.out.println("test");
+
 	}
 		
 }
