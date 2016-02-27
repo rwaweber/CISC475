e51377aead94cfe636938e7caed2683c1fe16f23
Ben Rodd <brodd@udel.edu>
2016-02-27 15:12:01 -0500

    more rearranging of test fields

--- src/tests/FileParserTest.java
+++ src/tests/FileParserTest.java
@@ -25,22 +25,22 @@ public class FileParserTest {
 	static List<Integer> ages = Arrays.asList(new Integer[]{32, 40, 27});
 	static List<Integer> ids = Arrays.asList(new Integer[]{12345, 98765, 11223});
 
-	static Stream<String> namesStream = names.stream();
-	static Stream<Integer> agesStream = ages.stream();
-	static Stream<Integer> idsStream = ids.stream();
-
-	static List<Stream> streams = Arrays.asList(new Stream[]{namesStream, agesStream, idsStream});
-
 	static int numRows = 3;
 
 
 	@Test
 	public void testJSONToStream() {
-	 
+
 	}
 
 	@Test
 	public void testStreamToJSON(){
+
+		Stream<String> namesStream = names.stream();
+		Stream<Integer> agesStream = ages.stream();
+		Stream<Integer> idsStream = ids.stream();
+
+		List<Stream> streams = Arrays.asList(new Stream[]{namesStream, agesStream, idsStream});
 		JSONObject[] jsonObjects = FileParser.streamToJSON(streams, colNames, numRows);
 		assertNotNull(jsonObjects);
 		assertEquals(jsonObjects.length, 3);
@@ -48,17 +48,17 @@ public class FileParserTest {
 		assertEquals(jsonObjects[1].get("Age"), 40);
 		assertEquals(jsonObjects[2].get("ID"), 11223);
 	}
-	
+
 	@Test
 	public void testCreateFile(){
-	
+
 		String fileName = "src/tests/testfiles/test.json";
 		FileParser.createFile(fileName);
-		
+
 		File createdFile = new File(fileName);
 		assertTrue(createdFile.exists());
 	}
-	
+
 	@Test
 	public void testGetJsonObjectList(){
 		JSONObject[] jsonObjects = FileParser.getJsonObjectList(10);
@@ -69,21 +69,27 @@ public class FileParserTest {
 
 	@Test
 	public void testWriteJSONToFile() throws ParseException, IOException{
+
+		Stream<String> namesStream = names.stream();
+		Stream<Integer> agesStream = ages.stream();
+		Stream<Integer> idsStream = ids.stream();
 		
+		List<Stream> streams = Arrays.asList(new Stream[]{namesStream, agesStream, idsStream});
 		JSONObject[] jsonObjects = FileParser.streamToJSON(streams, colNames, numRows);
-		
+
 		String fileName = "src/tests/testfiles/test.json";
 		FileParser.writeJSONToFile(fileName, jsonObjects);
 
 		File noFile = new File("src/tests/testfiles/nofile.json");
 		assertFalse(noFile.exists());
 	}
-	
+
 	@Test
 	public void testCSVToStream(){
-		
-		
-		
+		List<Stream> streams = FileParser.csvToStream(5, 10);
+		assertNotNull(streams);
+
+
 	}
 
 }
