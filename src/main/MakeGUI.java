339f4528bdaa84e3d127562bdebee2cf9af1ed25
gmohler213 <gmohler@udel.edu>
2016-03-08 12:46:25 -0500

    changed MakeGUI constructor to take parameters.

--- src/main/MakeGUI.java
+++ src/main/MakeGUI.java
@@ -9,13 +9,15 @@ import javax.swing.JTable;
 public class MakeGUI extends JFrame{
     
 	JTable tab;
-	String[] colHeaders = {"Name", "Age", "UDID"};
-	Object[][] cells = {{"Greg", 21, 702.123123},{"Ben", 53, 702.123124}, {"Will", 30, 702.123125}};
+	String[] colHeaders;
+	Object[][] cells;
 	private static int TABLE_WIDTH = 200;
 	private static int TABLE_HEIGHT = 250;
 	
-	public MakeGUI(){
+	public MakeGUI(String[] colHeaders, Object[][] cells){
 		
+		this.colHeaders = colHeaders;
+		this.cells = cells;
 		tab = new JTable(cells, colHeaders);
 		tab.setBounds(50, 50, TABLE_WIDTH, TABLE_HEIGHT);
 		JScrollPane pane = new JScrollPane(tab);
@@ -26,7 +28,9 @@ public class MakeGUI extends JFrame{
     
 
     public static void main(String[] args){
-    	new MakeGUI();
+    	String[] a = {"Name", "Age", "Student ID"}; 
+    	Object[][] b = {{"Greg", 30, 702.123123}, {"Will", 35, 702.123124}, {"Ben", 40, 702.123125}};
+    	new MakeGUI(a, b);
     }
     
    
