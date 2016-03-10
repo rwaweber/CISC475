5083a6a5b8e558ee47a93c35010b4b9e63268fcb
gmohler213 <gmohler@udel.edu>
2016-03-09 19:30:07 -0500

    MakeGUI now flips the rows and columns. Also EXIT_ON_CLOSE is now enabled.

--- src/main/MakeGUI.java
+++ src/main/MakeGUI.java
@@ -14,23 +14,36 @@ public class MakeGUI extends JFrame{
 	private static int TABLE_WIDTH = 200;
 	private static int TABLE_HEIGHT = 250;
 	
+	Object[][] newCells;
+	
 	public MakeGUI(String[] colHeaders, Object[][] cells){
 		
+		//flip 2D array:
+		newCells = new Object[cells[0].length][colHeaders.length];
+		for(int i=0; i<cells[0].length; i++){
+			for(int k=0; k<cells.length; k++){
+				newCells[i][k] = cells[k][i];
+			}
+		}
+		
+		//setup view:
 		this.colHeaders = colHeaders;
-		this.cells = cells;
-		tab = new JTable(cells, colHeaders);
+		this.cells = newCells;
+		tab = new JTable(newCells, colHeaders);
 		tab.setBounds(50, 50, TABLE_WIDTH, TABLE_HEIGHT);
 		JScrollPane pane = new JScrollPane(tab);
 		this.add(pane);
 		this.setSize(300,400);
-		this.setVisible(true);		
+		this.setVisible(true);	
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 	}
     
 
     public static void main(String[] args){
-    	String[] a = {"Name", "Age", "Student ID"}; 
-    	Object[][] b = {{"Greg", 30, 702.123123}, {"Will", 35, 702.123124}, {"Ben", 40, 702.123125}};
-    	new MakeGUI(a, b);
+    	String[] a = {"Name", "Age", "Student ID", "Male"}; 
+    	Object[][] b = {{"Greg", "Will", "Ben"}, {21, 35, 40}, {702.123123, 702.123124, 702.123125}, {true, true, true}};
+    	MakeGUI x = new MakeGUI(a, b);
     }
     
    
