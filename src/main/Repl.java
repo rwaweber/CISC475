7d28bb450f3259ff39a1f2517df0c2d6e8ac44ea
rwaweber <rwaweber@udel.edu>
2016-04-13 13:03:59 -0400

    1. added subprocess command building, for thigns like ls and pwd 2. also added InterruptExceptions, since these commands are running as subprocesses, need to be able to handle signals

--- src/main/Repl.java
+++ src/main/Repl.java
@@ -1,15 +1,25 @@
 package main;
 
 import java.lang.StringBuilder;
+import java.lang.ProcessBuilder;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import main.MakeGUI;
 import main.FileParser;
 import main.Records;
 
 public class Repl {
     boolean state = false;
+    static double version = 1.0;
+    static String usage = "Usage: [verb] [dependent set of arguments]\n";
+    static String versioninfo = "This cli data munging utility was created by undergraduate \n"
+	+ "Students in Spring 2016 of a CISC475 class. \n"
+	+ "Those students were: \n"
+	+ "Will Weber, Ben Rodd, Johanna Jan, Greg Mohler, and Teague Forren\n"
+	+ "And this is release version: " + version + ". ";
     
     public Repl() {
 	this.state = true;
@@ -17,7 +27,7 @@ public class Repl {
     
     // clear screen with escape code gibberish
     public static void clear() {
-	    System.out.print("\033[H\033[2J");
+	System.out.print("\033[H\033[2J");
     }
 
     // exit system
@@ -30,7 +40,7 @@ public class Repl {
     }
 		
     // consumes command sequence, should determine the root of the command tree
-    public static boolean parseCommand(String[] commands) throws IOException {
+    public static boolean parseCommand(String[] commands) throws IOException, InterruptedException {
 	String root = commands[0];
 	switch(root) {
 	case "quit" :
@@ -39,11 +49,22 @@ public class Repl {
 	case "break":
 	    quit(0);
 	    return true;
+	case "version":
+	    System.out.println(versioninfo);
+	    return true;
 	case "clear":
 	    clear();
 	    return true;
 	case "view":
 	    viewOrganizer(commands);
+	    return true;
+	case "" :
+	    return true;
+	case "pwd":
+	    pbBuild(commands);
+	    return true;
+	case "ls":
+	    pbBuild(commands);
 	    return true;
 	default:
 	    return false;
@@ -68,26 +89,52 @@ public class Repl {
 	    }
 	} else {
 	    // inform user of 
-	    System.out.println("incorrect argument count \nUsage: view [csvfile]\n");
+	    System.out.println("incorrect argument count \n Usage: view [csvfile]\n");
 	    return false;
 	}
     }
 	    
-    public static void printLoop() throws IOException {
+    public static void printLoop() throws IOException, InterruptedException {
 	Scanner scan = new Scanner(System.in);
 	try {
 	    System.out.print("> ");
 	    while(scan.hasNextLine()) {
 		String line = scan.nextLine().toLowerCase();
-		parseCommand(commandify(line));
-		System.out.print("> ");
+		if (parseCommand(commandify(line))) {
+		    System.out.print("> ");
+		} else {
+		    System.out.println("No command '" + line + "' found");
+		    System.out.print(usage);
+		    System.out.print("> ");
+		}
 	    }
 	} finally {
 	    scan.close();
 	}
     }
+
+    public static boolean pbBuild(String[] arguments) throws IOException, InterruptedException {
+	String line = "";
+	String output = "";
+	try {	    
+	    Process p = Runtime.getRuntime().exec(arguments);
+	    p.waitFor();
+	    BufferedReader buf = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+	    while ((line = buf.readLine()) != null) {
+		output += line + "\n";
+	    }
+	    System.out.println(output);
+	    return true;
+	}
+	catch(InterruptedException e) {
+	    e.getMessage();
+	    e.printStackTrace();
+	    return false;
+	}
+    }
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 	printLoop();
     }
 }
