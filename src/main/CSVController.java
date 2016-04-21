a0b0630bcfdf0a4c7be203a7c8a2cb5ceb8c72ef
Ben Rodd <brodd@udel.edu>
2016-04-21 17:37:18 -0400

    got rid of erroneous import

--- src/main/CSVController.java
+++ src/main/CSVController.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
-import org.supercsv.*;
 import org.supercsv.io.CsvBeanReader;
 import org.supercsv.io.CsvListReader;
 import org.supercsv.io.ICsvBeanReader;
