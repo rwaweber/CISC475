d7b9b8679ae1ed5ddbcf6aec01d017492ac97880
Ben Rodd <brodd@udel.edu>
2016-04-22 01:15:42 -0400

    added getRow method to CSVController

--- src/main/CSVController.java
+++ src/main/CSVController.java
@@ -6,8 +6,18 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.supercsv.cellprocessor.FmtBool;
+import org.supercsv.cellprocessor.FmtDate;
+import org.supercsv.cellprocessor.Optional;
+import org.supercsv.cellprocessor.constraint.LMinMax;
+import org.supercsv.cellprocessor.constraint.NotNull;
+import org.supercsv.cellprocessor.constraint.UniqueHashCode;
+import org.supercsv.cellprocessor.ift.CellProcessor;
 import org.supercsv.io.CsvBeanReader;
 import org.supercsv.io.CsvListReader;
 import org.supercsv.io.CsvListWriter;
@@ -25,37 +35,69 @@ public class CSVController {
 
 	public CSVController(String fileName) throws IOException{
 		this.fileName = fileName;
-		initReader();
-		initWriter();
 	}
 
 	private void initReader() throws FileNotFoundException {
 		reader = new CsvListReader(new FileReader(fileName), CsvPreference.STANDARD_PREFERENCE);
 	}
 	
+	private void closeReader() throws IOException{
+		reader.close();
+	}
+	
 	private void initWriter() throws IOException {
 		writer = new CsvListWriter(new FileWriter(fileName, true), CsvPreference.STANDARD_PREFERENCE);
 	}
-
+	
+	private void closeWriter() throws IOException{
+		writer.close();
+	}
+	
 	public List<String> getRow(int rowIndex) throws IOException{
-		for(int i = 0; i < rowIndex - 1; i++){
-			reader.read();
+		initReader();
+		List<String> thisRow;
+		int thisIndex = 0;
+		while((thisRow = reader.read()) != null && thisIndex < rowIndex){
+			thisIndex++;
 		}
-		return reader.read();
+		closeReader();
+		return thisRow;
 	}
 
 	public List<String> getCol(int colIndex) throws IOException {
+		initReader();
 		ArrayList<String> col = new ArrayList<String>();
 		List<String> thisRow;
 		while((thisRow = reader.read()) != null){
 			col.add(thisRow.get(colIndex));
 		}		
+		closeReader();
 		return col;
 	}
 	
+
+	public void addRow(List<String> row) throws IOException {
+		initWriter();
+		writer.write(row);
+		closeWriter();
+
+	}
+
 	public static void main(String args[]) throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader("src/tests/testfiles/testNumeric.csv"));
 		System.out.println(br.readLine());
+	}
+
+	public List<String> getLastRow() throws IOException {
+		initReader();
+		List<String> thisRow = reader.read();
+		List<String> lastRow = thisRow;
+		while(thisRow != null){
+			lastRow = thisRow;
+			thisRow = reader.read();
+		}
+		closeReader();
+		return lastRow;
 	}
 
 }
--- src/tests/CSVControllerTest.java
+++ src/tests/CSVControllerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -14,6 +15,8 @@ public class CSVControllerTest {
 
 	static String numericFile = "src/tests/testfiles/testNumeric.csv";
 	static String destFile = "src/tests/testfiles/testWrite.csv";
+	
+	static List<String> firstRow = Arrays.asList(new String[]{"Bob", "Benson", "NY", "12345"});
 
 	@Test
 	public void testGetRow() throws IOException {
@@ -23,7 +26,22 @@ public class CSVControllerTest {
 		assertEquals(firstRow.size(), 4);
 		assertEquals(firstRow.get(0), "Count");
 		assertEquals(firstRow.get(3), "Distinct");
+		List<String> middleRow = csvController.getRow(5);
+		assertNotNull(middleRow);
+		assertEquals(middleRow.size(), 4);
+		assertEquals(middleRow.get(0), "5");
+		assertEquals(middleRow.get(3), "4");
 		List<String> lastRow = csvController.getRow(10);
+		assertNotNull(lastRow);
+		assertEquals(lastRow.size(), 4);
+		assertEquals(lastRow.get(0), "10");
+		assertEquals(lastRow.get(3), "3");
+	}
+	
+	@Test
+	public void testGetLastRow() throws IOException{
+		CSVController csvController = new CSVController(numericFile);
+		List<String> lastRow = csvController.getLastRow();
 		assertNotNull(lastRow);
 		assertEquals(lastRow.size(), 4);
 		assertEquals(lastRow.get(0), "10");
@@ -40,6 +58,16 @@ public class CSVControllerTest {
 		for(int i = 1; i < 11; i++){
 			assertEquals(firstCol.get(i), ((Integer)i).toString());
 		}
+	}
+	
+	@Test
+	public void testAddRow() throws IOException{
+		CSVController csvController = new CSVController(destFile);
+		csvController.addRow(firstRow);
+		List<String> addedRow = csvController.getRow(0);
+		assertNotNull(addedRow);
+		assertEquals(addedRow.size(), 4);
+		assertEquals(addedRow, firstRow);
 	}
 	
 
