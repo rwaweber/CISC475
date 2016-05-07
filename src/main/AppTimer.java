6d8f7d06c93b5cf5664063fb44af0fa4f5e76323
Ben Rodd <brodd@udel.edu>
2016-05-06 21:23:39 -0400

    finished AppTimer class that can be used to time the application

--- src/main/AppTimer.java
+++ src/main/AppTimer.java
@@ -7,18 +7,45 @@ import java.util.concurrent.TimeUnit;
 
 public class AppTimer {
 
+	private long startTime;
+	private long endTime;
+
 	public String getStringTime(long time) {
-//		Date date = new Date(time);
-//		DateFormat formatter = new SimpleDateFormat("mm:ss:SSS");
-//		String stringTime = formatter.format(date);
-//		return stringTime;
 		return String.format("%d min, %d sec", 
-			    TimeUnit.MILLISECONDS.toMinutes(time),
-			    TimeUnit.MILLISECONDS.toSeconds(time) - 
-			    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(time))
-			);
+				TimeUnit.MILLISECONDS.toMinutes(time),
+				TimeUnit.MILLISECONDS.toSeconds(time) - 
+				TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(time))
+				);
 	}
-	
-	
+
+	public void startTimer() {
+		setStartTime(System.currentTimeMillis());
+	}
+
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public void endTimer() {
+		setEndTime(System.currentTimeMillis());
+	}
+
+	public long getEndTime() {
+		return endTime;
+	}
+
+	public void setStartTime(long startTime) {
+		this.startTime = startTime;
+	}
+
+	public void setEndTime(long endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getElapsedTime() {
+		return getStringTime(endTime - startTime);
+	}
+
+
 
 }
--- src/tests/AppTimerTest.java
+++ src/tests/AppTimerTest.java
@@ -2,27 +2,70 @@ package tests;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 import main.AppTimer;
 
 public class AppTimerTest {
 
+	long startTime = 1800000 + 1920000 + 29000 + 373;
+	long endTime = startTime + (60000 * 22) + (1000 * 45);
+
 	@Test
 	public void testConstructor() {
 		AppTimer timer = new AppTimer();
 		assertNotNull(timer);
 	}
-	
+
 	@Test
 	public void testGetStringTime(){
 		AppTimer timer = new AppTimer();
-		long time = 1800000 + 1920000 + 29000 + 373;
-		System.out.println(time);
-		String stringTime = timer.getStringTime(time);
-		System.out.println(stringTime);
+		String stringTime = timer.getStringTime(startTime);
 		assertNotNull(stringTime);
 		assertEquals(stringTime, "62 min, 29 sec");
+	}
+
+	@Test
+	public void testStartTimer(){
+		AppTimer timer = new AppTimer();
+		timer.startTimer();
+		long startTime = timer.getStartTime();
+		assertNotNull(startTime);
+		assertNotEquals(startTime, 0);
+	}
+
+	@Test
+	public void testEndTimer(){
+		AppTimer timer = new AppTimer();
+		timer.endTimer();
+		long endTime = timer.getEndTime();
+		assertNotNull(endTime);
+		assertNotEquals(endTime, 0);
+	}
+	
+	@Test
+	public void testSetStartTime(){
+		AppTimer timer = new AppTimer();
+		timer.setStartTime(1000l);
+		assertEquals(timer.getStartTime(), 1000);
+	}
+	
+	@Test
+	public void testSetEndTime(){
+		AppTimer timer = new AppTimer();
+		timer.setEndTime(1000l);
+		assertEquals(timer.getEndTime(), 1000);
+	}
+	
+	@Test
+	public void getElapsedTime(){
+		AppTimer timer = new AppTimer();
+		timer.setStartTime(startTime);
+		timer.setEndTime(endTime);
+		String elapsedTime = timer.getElapsedTime();
+		assertEquals(elapsedTime, "22 min, 45 sec");
+		
 	}
 
 }
